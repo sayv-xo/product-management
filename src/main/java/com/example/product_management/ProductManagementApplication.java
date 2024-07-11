4dcbcc65e6bde66aad8913ea74c125bf24240aaf
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-07-11 21:14:43 +0000

    initial setup

---
+++ src/main/java/com/example/product_management/ProductManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.product_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class ProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+
+}
