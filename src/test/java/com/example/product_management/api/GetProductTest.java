8f74d040d953f8cc7a48913ecdf9d3401e0a65c2
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-09-20 23:49:54 +0000

    test category

--- src/test/java/com/example/product_management/api/GetProductTest.java
+++ src/test/java/com/example/product_management/api/GetProductTest.java
@@ -2,16 +2,52 @@ package com.example.product_management.api;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class GetProductTest {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+    @Test
+    public void testCreateCategory() {
+        String categoryName = "New Category";
+
+        String requestBody = "{ \"categoryName\": \"" + categoryName + "\" }";
+
+        given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/admin/categories")
+                .then()
+                .statusCode(200);
+    }
+
     @Test
     public void testStatusCodeAndContentType() {
         when().
-                get("http://localhost:8080/api/public/products").
+                get("/api/public/products").
                 then().
                 assertThat().
                 statusCode(200).
                 contentType("application/json");
+    }
+
+    @Test
+    public void testGetCategory() {
+        when().
+                get("/api/public/categories").
+                then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json").
+                body("categoryName", hasItem(equalTo("New Category")));
     }
 }
