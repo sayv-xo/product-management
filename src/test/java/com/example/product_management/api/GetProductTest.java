db9065fc58e786ca7bf0cd501a4b7adc9637ecc7
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-09-20 17:08:39 +0000

    add test

---
+++ src/test/java/com/example/product_management/api/GetProductTest.java
@@ -0,0 +1,22 @@
+package com.example.product_management.api;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GetProductTest {
+
+
+    @Test
+    public void getProduct() {
+        when().
+                get("http://localhost:8080/api/public/products").
+                then().
+                assertThat().
+                body("category", hasItem("Fashion"));
+    }
+
+}
